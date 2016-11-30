72f2850cfffa1d1c672e4ff60f6a2cd3e33a774f
O'Donnell <odjohnny@amazon.com>
2016-11-29 20:15:22 -0800

    Using filesets

--- src/Find.java
+++ src/Find.java
@@ -1,33 +1,59 @@
 import org.apache.tools.ant.Task;
 import org.apache.tools.ant.BuildException;
+import org.apache.tools.ant.types.FileSet;
+import org.apache.tools.ant.DirectoryScanner;
+
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Iterator;
+
+import java.io.File;
 
 public class Find extends Task {
 
-    private String property;
-    private String value;
-    private String print;
+    /* Using filesets */
+    private String file;
+    private String location;
+    private List<FileSet> filesets = new ArrayList<>();
 
-    public void setProperty(String property) {
-        this.property = property;
+    /* Using filesets */
+    public void setFile(String file) {
+    	this.file = file;
     }
 
-    public void setValue(String value) {
-    	this.value = value;
+    public void setLocation(String location) {
+    	this.location = location;
     }
 
-    public void setPrint(String print) {
-    	this.print = print;
+    public void addFileset(FileSet fileset) {
+	this.filesets.add(fileset);
+    }
+
+    protected void validate() {
+        if (file==null) throw new BuildException("file not set");
+        if (location==null) throw new BuildException("location not set");
+        if (filesets.size()<1) throw new BuildException("fileset not set");
     }
 
     public void execute() {
-        if (print != null) {
-            String propValue = getProject().getProperty(print);
-            log(propValue);
-        } else {
-            if (property == null) throw new BuildException("property not set");
-            if (value    == null) throw new BuildException("value not set");
-            getProject().setNewProperty(property, value);
+	validate();                                                             // 1
+        String foundLocation = null;
+        for(Iterator itFSets = filesets.iterator(); itFSets.hasNext(); ) {      // 2
+            FileSet fs = (FileSet)itFSets.next();
+            DirectoryScanner ds = fs.getDirectoryScanner(getProject());         // 3
+            String[] includedFiles = ds.getIncludedFiles();
+            for(int i=0; i<includedFiles.length; i++) {
+                String filename = includedFiles[i].replace('\\','/');           // 4
+                filename = filename.substring(filename.lastIndexOf("/")+1);
+                if (foundLocation==null && file.equals(filename)) {
+                    File base  = ds.getBasedir();                               // 5
+                    File found = new File(base, includedFiles[i]);
+                    foundLocation = found.getAbsolutePath();
+                }
+            }
         }
+        if (foundLocation!=null)                                                // 6
+            getProject().setNewProperty(location, foundLocation);
     }
 }
 
--- src/FindTest.java
+++ src/FindTest.java
@@ -4,7 +4,6 @@ import org.junit.Before;
 import org.junit.Assert;
 import org.apache.tools.ant.BuildFileRule;
 
-
 public class FindTest {
 
     @Rule
@@ -17,9 +16,60 @@ public class FindTest {
     }
 
     @Test
-    public void testSimple() {
-        buildRule.executeTarget("use.simple");
-        Assert.assertEquals("test-value", buildRule.getLog());
+    public void testMissingFile() {
+        Find find = new Find();
+        try {
+            find.execute();
+            Assert.fail("No 'no-file'-exception thrown.");
+        } catch (Exception e) {
+            // exception expected
+            String expected = "file not set";
+            Assert.assertEquals("Wrong exception message.", expected, e.getMessage());
+        }
+    }
+
+    @Test
+    public void testMissingLocation() {
+        Find find = new Find();
+        find.setFile("ant.jar");
+        try {
+            find.execute();
+            Assert.fail("No 'no-location'-exception thrown.");
+        } catch (Exception e) {
+			// exception expected
+            String expected = "location not set";
+            Assert.assertEquals("Wrong exception message.", expected, e.getMessage());
+        }
+    }
+
+    @Test
+    public void testMissingFileset() {
+        Find find = new Find();
+        find.setFile("ant.jar");
+        find.setLocation("location.ant-jar");
+        try {
+            find.execute();
+            Assert.fail("No 'no-fileset'-exception thrown.");
+        } catch (Exception e) {
+			// exception expected
+            String expected = "fileset not set";
+            Assert.assertEquals("Wrong exception message.", expected, e.getMessage());
+        }
+    }
+
+    @Test
+    public void testFileNotPresent() {
+        buildRule.executeTarget("testFileNotPresent");
+        String result = buildRule.getProject().getProperty("location.ant-jar");
+        Assert.assertNull("Property set to wrong value.", result);
+    }
+
+    @Test
+    public void testFilePresent() {
+        buildRule.executeTarget("testFilePresent");
+        String result = buildRule.getProject().getProperty("location.ant-jar");
+        Assert.assertNotNull("Property not set.", result);
+        Assert.assertTrue("Wrong file found.", result.endsWith("ant.jar"));
     }
 }
 
